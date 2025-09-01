222324be5780e8d0e97d58601ac40cfbfc7e7226
GibbatronGiblet <lbgibson@students.unwsp.edu>
2025-09-01 14:45:26 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -4,4 +4,5 @@ def proverbs_22_29():
     print("He will stand before kings;")
     print('he will not stand obscure men."')
     print('      (Proverbs 22:29)')
+#Written 9/1/25 by Logan Gibson. The program's name is "Proverbs 22:29"
 proverbs_22_29()
